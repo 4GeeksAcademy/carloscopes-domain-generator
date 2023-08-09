720c7d82a084cfb9244fe9d800310689c2c32224
carloscopes <carlos.mcp.97@gmail.com>
2023-08-09 18:21:27 -0400

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "your"];
+  let adj = ["best", "worst", "great"];
+  let noun = ["burger", "pizza", "pasta"];
+  let ext = [".com", ".net"];
+
+  function generarDominios(pronombres, adjetivos, sustantivos, extensiones) {
+    const resultado = [];
+
+    let dominio;
+
+    pronombres.forEach(function(pronombre) {
+      adjetivos.forEach(function(adjetivo) {
+        sustantivos.forEach(function(sustantivo) {
+          extensiones.forEach(function(extension) {
+            dominio = `${pronombre}${adjetivo}${sustantivo}${extension}`;
+            resultado.push(dominio);
+          });
+        });
+      });
+    });
+
+    return resultado;
+  }
+
+  const dominios = generarDominios(pronoun, adj, noun, ext);
+
+  const lista = document.getElementById("list");
+
+  dominios.forEach(dominios => {
+    let li = document.createElement("li");
+    let nodoTexto = document.createTextNode(dominios);
+    li.appendChild(nodoTexto);
+    li.className = "domainItems";
+    lista.appendChild(li);
+  });
 };
